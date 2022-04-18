1ece0c671f39b06bb3ad3d183b721258f3c1ee92
Archili Zivzivadze <Archili_Zivzivadze@epam.com>
2022-04-18 13:49:06 +0400

    asd

---
+++ models/FeedbackModel.py
@@ -0,0 +1,5 @@
+class FeedbackModel:
+    def __init__(self, file_id, file_name, question_id):
+        self.FileId = file_id
+        self.FileName = file_name
+        self.QuestionId = question_id
