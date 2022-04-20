d4cc07e9d9ee173500cc3f04098895d5c6ffe4b1
Archili Zivzivadze <Archili_Zivzivadze@epam.com>
2022-04-20 19:30:17 +0400

    update

--- src/domains/prepareNotifications.py
+++ src/domains/prepareNotifications.py
@@ -19,9 +19,9 @@ def process_notification(params: Parameters):
         box_files = box_service.download_reports(params.ReportSubPath)
         feedback_files = excel_service.prepare_notifications_file(box_files, params)
         json_data = json.dumps(feedback_files)
-
+        test_value = 'defined inside python'
         print('##vso[task.setvariable variable=json_data;]%s' % json_data)
-
+        print('##vso[task.setvariable variable=test_value;]%s' % test_value)
     except BaseException as bs:
         print(bs)
 
@@ -31,7 +31,7 @@ def process_finish(params: Parameters):
 
     # feedback_delivered = send_notification()
     feedback_files = json.loads(params.feedback_files)
-    feedback_delivered = feedback_files
+    feedback_delivered = feedback_files 
     excel_service.complete_send_feedback(feedback_delivered, params)
     box_service.update_contents_to_box(feedback_files, params.ReportSubPath)
 
