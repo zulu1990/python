c820b5df600dcaee0a472b9b482bac3d34a2a623
Archili Zivzivadze <Archili_Zivzivadze@epam.com>
2022-04-20 21:55:54 +0400

    adsd

--- src/services/parametersService.py
+++ src/services/parametersService.py
@@ -5,7 +5,9 @@ from models.Parameters import Parameters
 
 def parse_parameters():
     parser = ArgumentParser()
-
+    
+    parser.add_argument('--execute', help='determines which part of application to run', type=str, default='NOTIFICATION')
+    parser.add_argument('--feedback_files', help='sent feedback list', type=str, default='')
     parser.add_argument('--notification_template', help='notification template file name', type=str, default='notification_template.xlsx')
     parser.add_argument('--report_template', help='report template file name', type=str, default='report_template.xlsx')
     parser.add_argument('--feedback_query', help='feedback query function name', type=str, default= 'GetUserFeedbacksAutomation()')
@@ -16,8 +18,7 @@ def parse_parameters():
     parser.add_argument('--feedback_complete_mark', help='mark which indicates feedback was sent', type=str, default='completed_feedback_sent')
     parser.add_argument('--formula', help='excel formula to validate statuses', type=str, default='=fields!$A$2:$A$9')
     parser.add_argument('--formula_range', help='column range to apply --formula', type=str, default='A2:A1048576')
-    parser.add_argument('--execute', help='determines which part of application to run', type=str, default='NOTIFICATION')
-    parser.add_argument('--feedback_files', help='sent feedback list', type=str, default='')
+    
     
     params = Parameters()
     args = parser.parse_args()
