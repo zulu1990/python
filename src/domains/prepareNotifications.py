34744bf0da78a3612d587fa4b43622de92cba30b
Archili Zivzivadze <Archili_Zivzivadze@epam.com>
2022-04-21 11:30:55 +0400

    asd

--- src/domains/prepareNotifications.py
+++ src/domains/prepareNotifications.py
@@ -21,10 +21,10 @@ def process_notification(params: Parameters):
         box_files = box_service.download_reports(params.ReportSubPath)
         feedback_files = excel_service.prepare_notifications_file(box_files, params)
         json_data = json.dumps(feedback_files)
-        test_value = 'defined inside python'
+        test_value = [{"FieldId": "941393029933", "FileName": "2022-3-Report.xlsx", "QuestionIds": []}, {"FieldId": "947082945940", "FileName": "2022-4-Report.xlsx", "QuestionIds": ["02910bf1b2d340df966539863f3c0cd6", "842045f395f947d8919edf66f4aefd50", "6748abae3aaa4727aa271d2a44043cc4", "5a3014ef3e6345e3884b1e85833d1ad9"]}]
         print("json data {0}".format(json_data))
-        print(r'##vso[task.setvariable variable=json_data]{0}'.format(json_data))
-        print(r'##vso[task.setvariable variable=test_value]{0}'.format(json_data))
+        print('##vso[task.setvariable variable=json_data]{0}'.format(json_data))
+        print('##vso[task.setvariable variable=test_value]{0}'.format(test_value))
 
     except BaseException as bs:
         print(bs)
